5b029972bf911bc228a2911682c1e02496251897
Neo <binishjoshi@pm.me>
2023-06-05 11:55:54 +0545

    saveFile accepts a `path` argument, where it stores the file

--- src/lib/save-file.ts
+++ src/lib/save-file.ts
@@ -3,13 +3,22 @@ import { writeFileSync } from 'fs';
 
 import { FileType } from '../types/file.type';
 
-export function saveFile(file: FileType) {
+/**
+ * Saves the `file` in `path` with file's MD5 hash as its filename
+ * @param file
+ * @param path
+ * @returns path to the file e.g. `path` + `fileName`, where `fileName` is the MD5 hash of the `file`
+ */
+export function saveFile(
+  file: FileType,
+  path: 'uploads/images' | 'uploads/audio/lossless' | 'uploads/audio/lossy',
+) {
   const md5Hash = createHash('md5');
   md5Hash.update(file.buffer);
   const hashText = md5Hash.digest('hex');
 
   const fileName = `${hashText}.${file.ext}`;
-  const filePath = 'uploads/images/' + fileName;
+  const filePath = path + fileName;
   writeFileSync(filePath, file.buffer);
 
   return filePath;
