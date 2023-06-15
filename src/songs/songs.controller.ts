626806dc60d13c465c32b8dbcaea008ebd6e98e3
Neo <binishjoshi@pm.me>
2023-06-15 16:00:10 +0545

    fetch song url as user  need to write e2e test

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+  constructor(private songsSerivce: SongsService) {}
+
+  @Get('/url/:id')
+  @UseGuards(AuthGuard)
+  async getSongUrl(@Param('id') id: string, @CurrentUser() user: User) {
+    const url = await this.songsSerivce.getSongUrl(id, user);
+    return { url };
+  }
+}
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -3,8 +3,25 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Song } from './song.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class SongsService {
   constructor(@InjectRepository(Song) private repo: Repository<Song>) {}
+
+  findById(id: string) {
+    return this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async getSongUrl(id: string, user: User) {
+    const song = await this.findById(id);
+
+    const url = user.preference === 'flac' ? song.pathLossless : song.pathLossy;
+
+    return url;
+  }
 }
