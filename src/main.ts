dcc8e8301cdb993e723ad002a2761b1bae204fda
Neo <binishjoshi@pm.me>
2023-07-08 12:21:47 +0545

    serve image files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads/images'), {
+    prefix: '/uploads/images/',
+  });
+
   await app.listen(1337);
 }
 bootstrap();
